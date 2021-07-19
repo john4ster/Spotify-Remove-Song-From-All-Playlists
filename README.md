2e54663199e9902c02b2316a483f91265b63c59c
john4ster <johnforsterbusiness@gmail.com>
2021-07-19 19:42:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Remove Song From All Spotify Playlists
+Spotify has no option to remove a song from all your playlists. You have to manually go through each playlist and remove the song. This can be an annoying process, and can even lead to you forgetting to remove the song from some playlists. So I made this simple python script that you can use to remove a song from all your Spotify playlists at once.
+
+# How to use
+Simply run the command `python main.py "[Insert Song Name Here]" "[Insert Artist Name Here]"`. Make sure to put the song name in quotes. It is best to open Spotify and find the song first so you can enter its name exactly as it appears in Spotify.
+
+# Things to look out for
+Spaces: For songs with a space in them, you must put the song name in quotes, if you don't, the script will take each word as a seperate argument and will not be able to find the song.
+
+### Older Songs/Remasters
+Some older songs may have Remastered somewhere in the title, if this is not added the script will not find the song. For example, take "Bennie and the Jets" by Elton John. If you type "Bennie and the Jets" as the argument, but Spotify calls the song "Bennie and the Jets - 2014 Remastered", the script will not be able to find the song. Make sure to look at the song in Spotify and then copy the exact title as the argument for this script.
+
+### Artist Features
+Some songs also feature select artists and put their name in the title in parenthesis using feat. or ft. For example, take "See You Again" by Wiz Khalifa. If you type "See You Again" as the argument, but Spotify calls the song "See You Again (feat Charlie Puth)", the script will not be able to find the song. Remember to watch out for these as well, as they are technically part of the song title and must be included in the argument.
+
+### Case sensitive
+Remember to capitalize song names however they are capitalized in the Spotify title.
---
+++ main.py
@@ -0,0 +1,57 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import sys
+
+CLIENT_ID = "a663cbeb91af41109c2099889739ac3e"
+CLIENT_SECRET = "cb3219f342a149c5a123c10217122612"
+REDIRECT_URI = "https://removefromplaylists"
+
+#Spotify api scopes to read and write to playlists
+read_scope = "user-library-read"
+write_scope = "playlist-modify-public"
+#Command line arguments
+song = sys.argv[1]
+artist = sys.argv[2]
+
+#Song found is false by default, will change to true if the song is found in any playlists
+song_found = False
+
+#Set up scopes
+public_read = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                        client_secret=CLIENT_SECRET,
+                                                        redirect_uri=REDIRECT_URI,
+                                                        scope=read_scope))
+public_modify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                                        client_secret=CLIENT_SECRET,
+                                                        redirect_uri=REDIRECT_URI,
+                                                        scope=write_scope))
+
+#Playlist results
+public_results = public_read.current_user_playlists()
+#Offset used to get the playlist_id rather than the full uri of each playlist, each uri is returned 
+#as spotify:playlist:[playlist_id], so this offset will be used to just get the playlist_id
+id_offset = 17
+#Loop through each playlist
+print("Attempting to remove song from all playlists...")
+for playlist in public_results["items"]:
+  #Get the id of each playlist
+  current_playlist_id = playlist["uri"][id_offset:]
+  #Get items in each playlist
+  playlist_tracks = public_read.user_playlist_tracks(user=public_read.user, playlist_id=current_playlist_id)
+  #Get name of each track in each playlist
+  for track in playlist_tracks["items"]:
+    #Get each track's name and first listed artist to compare against arguments
+    track_name = track["track"]["name"]
+    track_artist = track["track"]["artists"][0]["name"]
+    #Get track's uri to remove it if it matches the arguments
+    track_uri= track["track"]["uri"]
+    #If the song was found, remove it from the playlist
+    if song == track_name and artist == track_artist:
+      public_modify.user_playlist_remove_all_occurrences_of_tracks(user=public_read.user, playlist_id=current_playlist_id, tracks=[track_uri])
+      song_found = True
+#Inform the user whether the song was found or not
+if song_found:
+  print("Song successfuly deleted from all playlists")
+else:
+  print("Song not found, please make sure you are entering the exact title.")
+  print("Check the README for examples of common argument errors.")
